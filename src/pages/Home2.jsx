e6d660ce249b7a61a892afc15aa87cf3f440a507
Dani-Bcn <nneodani@gmail.com>
2024-06-14 01:41:40 +0200

    test responsibe for mobile

--- src/pages/Home2.jsx
+++ src/pages/Home2.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import gsap from "gsap";
 import { motion as m } from "framer-motion";
+import { ScrollTrigger } from "gsap/all";
 import back_Img from "/assets/home-back.jpg";
 
 export const Home2 = (props) => {
   const [buttonServices, setButtonServices] = useState(false);
+ gsap.registerPlugin(ScrollTrigger)
 
   buttonServices
     ? gsap.to(".buttonServices", {
@@ -16,6 +18,11 @@ export const Home2 = (props) => {
 
   useEffect(() => {
     gsap.to(".years", {
+        scrollTrigger:{
+            trigger:".years",
+            stgart:"center center",
+            stop:1500,
+        },
       innerText: 15,
       duration: 2,
       ease: "circ.out",
@@ -24,18 +31,28 @@ export const Home2 = (props) => {
       },
     });
     gsap.to(".clients", {
+        scrollTrigger:{
+            trigger:".clients",
+            stgart:"center 500",
+            stop:50,
+        },
       innerText: 1000,
       delay: 0.5,
-      duration: 2,
+      duration: 1,
       ease: "circ.out",
       snap: {
         innerText: 5,
       },
     });
     gsap.to(".training", {
+        scrollTrigger:{
+            trigger:".training",
+            stgart:"center center",
+            stop:1500,
+        },
       innerText: 38000,
       delay: 1,
-      duration: 2,
+      duration: 1,
       ease: "circ.out",
       snap: {
         innerText: 5,
@@ -49,8 +66,12 @@ export const Home2 = (props) => {
         alt="img-back"
         className="absolute object-cover brightness-[0.3] blur-[2.5px]"
       />
-      <section className="relative w-[90vw] flex flex-col justify-center gap-10 items-center mt-40 p-3 ">
-        <m.div className=" overflow-hidden text-8xl mt-5 font-bold italic  ">
+      <section className="box relative w-[90vw] flex justify-center  gap-10 items-center mt-32 p-3 ">
+        <m.div className="wrapper-box flex flex-col gap-10  overflow-hidden text-8xl  font-bold italic  ">
+        <h3 className="text-xl mt-5">
+            <span>PEQUEÑOS CAMBIOS, GRANDES RESULTADOS.</span>
+          </h3>
+          <div className="overflow-hidden">
           <m.h3
             animate={{
               y: [550, 0],
@@ -59,12 +80,33 @@ export const Home2 = (props) => {
                 ease: "circOut",
               },
             }}
-            className="font-bold italic text-white text-center"
+            className="font-bold italic text-white "
           >
             WALK ON <span>FITNESS</span>
           </m.h3>
+          </div>
+          <div className="w-10/12  text-white  text-xl font-semibold ">
+            <p>
+              En Walk On Fitness, nos dedicamos a potenciar tu bienestar a
+              través de programas integrales de entrenamiento, nutrición y
+              fisioterapia. Descubre cómo podemos ayudarte a alcanzar tus
+              objetivos de salud y forma física.
+            </p>
+          </div>
+          
+
+          <button
+            onPointerOver={() => setButtonServices(true)}
+            onPointerOut={() => setButtonServices(false)}
+            className="w-40 rounded-md overflow-hidden h-16 text-white hover:shadow-[0px_0px_25px] shadow-white hover:text-orange-400 hover:bg-white italic text-3xl bg-orange-400   transiton-all duration-500 z-[200]"
+          >
+            <div className=" buttonServices flex -ml-[175px] gap-40 ">
+              <h3>Ver</h3>
+              <h3>Servicios</h3>
+            </div>
+          </button>
         </m.div>
-        <div className="box w-screen gap-5 flex mt-10 justify-center items-center ">
+        <div className=" w-screen gap-14 h-[500px] flex flex-col  justify-start ">
           <div className=" flex flex-col  justify-center  items-center  text-6xl font-bold ">
             <span className="flex">
               <h2 className="years z-[200]"></h2>
@@ -89,28 +131,6 @@ export const Home2 = (props) => {
             <h2 className="text-2xl italic">ENTRENAMIENTOS REALIZADOS</h2>
           </div>
         </div>
-        <h3 className="text-2xl text-center mt-5">
-          <span>PEQUEÑOS CAMBIOS, GRANDES RESULTADOS.</span>
-        </h3>
-
-        <div className="w-10/12 mt-10 text-white  text-xl font-semibold ">
-          <p>
-            En Walk On Fitness, nos dedicamos a potenciar tu bienestar a través
-            de programas integrales de entrenamiento, nutrición y fisioterapia.
-            Descubre cómo podemos ayudarte a alcanzar tus objetivos de salud y
-            forma física.
-          </p>
-        </div>
-        <button
-          onPointerOver={() => setButtonServices(true)}
-          onPointerOut={() => setButtonServices(false)}
-          className="w-40 rounded-md overflow-hidden h-16 text-white hover:shadow-[0px_0px_25px] shadow-white hover:text-orange-400 hover:bg-white italic text-3xl bg-orange-400   transiton-all duration-500 z-[200]"
-        >
-          <div className=" buttonServices flex  -ml-[175px] gap-40 ">
-            <h3>Ver</h3>
-            <h3>Servicios</h3>
-          </div>
-        </button>
       </section>
     </main>
   );
